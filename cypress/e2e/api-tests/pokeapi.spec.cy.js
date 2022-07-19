2a6d66b8ecad67ebec8c6b441f1149709ed026d1
abalabushko <anna.balabushko@gmail.com>
2022-07-19 08:30:45 +0200

    response validation

--- cypress/e2e/api-tests/pokeapi.spec.cy.js
+++ cypress/e2e/api-tests/pokeapi.spec.cy.js
@@ -10,8 +10,19 @@ describe('POKE REST API Test with Cypress', () => {
 
     it('API test - validate status', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
-        cy.get('@pokemon').its('status').should('eq', 200)
+        cy.get('@pokemon')
+            .its('status')
+            .should('eq', 200)
     })
+
+    it('API Test - validate name value', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon')
+            .its('body')
+            .should('include', {name: 'pikachu'})
+
+    })
+
 })
 
     
