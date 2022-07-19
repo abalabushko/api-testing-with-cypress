f2bea567467c0d318928244cbe5a26caeeee3d48
abalabushko <anna.balabushko@gmail.com>
2022-07-19 08:19:07 +0200

    add test for poke api and update gitignore

---
+++ cypress/e2e/api-tests/pokeapi.spec.cy.js
@@ -0,0 +1,12 @@
+describe('POKE REST API Test with Cypress', () => {
+    
+    it('API test -validate headers' , () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+    })
+})
+
+    
