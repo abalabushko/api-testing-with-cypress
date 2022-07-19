5bb84567b3b2bfa6a52fd387f1ee3162d2f837b2
abalabushko <anna.balabushko@gmail.com>
2022-07-19 08:25:02 +0200

    add test status code

--- cypress/e2e/api-tests/pokeapi.spec.cy.js
+++ cypress/e2e/api-tests/pokeapi.spec.cy.js
@@ -1,11 +1,16 @@
 describe('POKE REST API Test with Cypress', () => {
     
-    it('API test -validate headers' , () => {
+    it('API test - validate headers' , () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
         cy.get('@pokemon')
             .its('headers')
             .its('content-type')
             .should('include', 'application/json; charset=utf-8')
+    })
+
+    it('API test - validate status', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon').its('status').should('eq', 200)
     })
 })
 
