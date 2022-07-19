7da0aa247dae3994cac8d18bd79653e08776e98d
abalabushko <anna.balabushko@gmail.com>
2022-07-19 18:18:02 +0200

    add test with 404

--- cypress/e2e/api-tests/pokeapi.spec.cy.js
+++ cypress/e2e/api-tests/pokeapi.spec.cy.js
@@ -1,6 +1,7 @@
 describe('POKE REST API Test with Cypress', () => {
     
     it('API test - validate headers' , () => {
+
         cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
         cy.get('@pokemon')
             .its('headers')
@@ -9,6 +10,7 @@ describe('POKE REST API Test with Cypress', () => {
     })
 
     it('API test - validate status', () => {
+
         cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
         cy.get('@pokemon')
             .its('status')
@@ -16,11 +18,24 @@ describe('POKE REST API Test with Cypress', () => {
     })
 
     it('API Test - validate name value', () => {
+
         cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
         cy.get('@pokemon')
             .its('body')
             .should('include', {name: 'pikachu'})
 
+    })
+
+    it('API Test - validate negative status code', () => {
+
+        cy.request({ 
+            method:'GET',
+            url:'https://pokeapi.co/api/v2/pokemon/2500',
+            failOnStatusCode:false
+        }).as('pokemon')
+        cy.get('@pokemon')
+            .its('status')
+            .should('eq', 404)
     })
 
 })
