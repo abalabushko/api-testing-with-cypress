52b4ec5231e2491fda9e37de29b9716b4ad2f8a7
abalabushko <anna.balabushko@gmail.com>
2022-08-12 18:55:20 +0200

    revert

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env:{
-    auth_token: '$AUTH0_SECRET'
+    auth_token: '0c5e8798de155e01f6d2dabf8f142542154dae5acce9cdf794f5e740e5e910e8'
   },
   e2e: {
     setupNodeEvents(on, config) {
