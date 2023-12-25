8ac42090170a911ef81790fa5ea2d6f25ddef293
Anna Balabushko <anna.balabushko92@gmail.com>
2023-12-25 13:31:40 +0100

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = defineConfig({
   env:{
-    auth_token: 'AUTH0_SECRET',
+    auth_token: process.env.AUTH_TOKEN,
   },
   e2e: {
     setupNodeEvents(on, config) {
